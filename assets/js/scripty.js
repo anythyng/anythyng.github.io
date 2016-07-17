ba10dc773d1b71570eb508dfc38aa0ee2c1a9c13
anythyng <andy.zimmelman@gmail.com>
2016-07-17 19:30:55 -0400

    added new files

---
+++ assets/js/scripty.js
@@ -0,0 +1,43 @@
+function loadDoc(tag, url) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+      document.getElementsByClassName(tag).innerHTML = xhttp.responseText;
+    }
+  };
+  xhttp.open("GET", url, true);
+  xhttp.send();
+}
+
+function getSubjects(){
+    var subs_arr = [];
+    var subs = loadDoc('title', 'https://www.reddit.com')
+    return subs_arr;
+}
+
+function getAs(){
+    var a_arr = []
+    
+    return a_arr;
+}
+
+function getImgs(){
+    var imgs_arr = [];
+    
+    return imgs_arr;
+}
+
+
+function loadSubjects(){
+    var h2_subs = document.getElementsByTagName('h2');
+    var imgs = document.getElementsByTagName('img');
+    var a = document.getElementsByTagName('a');
+    var subs_arr = getSubjects();
+    var a_arr = getAs();
+    var imgs_arr = getImgs();
+//    for(var i = 0; i <  h2_subs.length; i++){
+//        h2_subs[i].innerHTML = subs_arr[i];
+//        a.href = a_arr[i];
+//        imgs.src = imgs_arr[i];
+//    }
+}
