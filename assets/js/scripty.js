7626479e707e73bf9f3ca54b8f8d82c155aafba0
anythyng <andy.zimmelman@gmail.com>
2016-07-17 21:34:43 -0400

    updated js

--- assets/js/scripty.js
+++ assets/js/scripty.js
@@ -1,8 +1,23 @@
+function myFunction(xml) {
+  var i;
+  var xmlDoc = xml.responseXML;
+  var table="<tr><th>Artist</th><th>Title</th></tr>";
+  var x = xmlDoc.getElementsByTagName("CD");
+  for (i = 0; i <x.length; i++) {
+    table += "<tr><td>" +
+    x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue +
+    "</td><td>" +
+    x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue +
+    "</td></tr>";
+  }
+  document.getElementById("demo").innerHTML = table;
+}
+
 function loadDoc() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
-        alert(JSON.stringify(xhttp.responseText));
+        myFunction(xhttp.responseText);
     }
   };
   xhttp.open("GET", "people.xml", true);
