c9f375e67fec10b539301066c3bb6e2b95b6e2cb
anythyng <andy.zimmelman@gmail.com>
2016-07-17 21:38:23 -0400

    updated js

--- assets/js/scripty.js
+++ assets/js/scripty.js
@@ -1,16 +1,17 @@
 function myFunction(xml) {
-  var i;
-  var xmlDoc = xml.responseXML;
-  var table="<tr><th>Name</th><th>Age</th></tr>";
-  var x = xmlDoc.getElementsByTagName("doc");
-  for (i = 0; i <x.length; i++) {
-    table += "<tr><td>" +
-    x[i].getElementsByTagName("name")[0].childNodes[0].nodeValue +
-    "</td><td>" +
-    x[i].getElementsByTagName("age")[0].childNodes[0].nodeValue +
-    "</td></tr>";
-  }
-  document.getElementById("demo").innerHTML = table;
+    var i;
+    var xmlDoc = xml.responseXML;
+    alert(xmlDoc)
+    var table="<tr><th>Name</th><th>Age</th></tr>";
+    var x = xmlDoc.getElementsByTagName("doc");
+    for (i = 0; i <x.length; i++) {
+        table += "<tr><td>" +
+        x[i].getElementsByTagName("name")[0].childNodes[0].nodeValue +
+        "</td><td>" +
+        x[i].getElementsByTagName("age")[0].childNodes[0].nodeValue +
+        "</td></tr>";
+    }
+    document.getElementById("demo").innerHTML = table;
 }
 
 function loadDoc() {
