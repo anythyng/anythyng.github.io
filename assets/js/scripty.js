77180b19c9f6d3cd8daf603ab3877c2a272a8d82
anythyng <andy.zimmelman@gmail.com>
2016-07-17 21:33:06 -0400

    updated js

--- assets/js/scripty.js
+++ assets/js/scripty.js
@@ -2,7 +2,7 @@ function loadDoc() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
-        alert(xhttp);
+        alert(JSON.parse(xhttp.responseText));
     }
   };
   xhttp.open("GET", "people.xml", true);
