55e9c5f47a72f8ef66530c86334f89fb84f8ca05
anythyng <andy.zimmelman@gmail.com>
2016-07-17 21:50:58 -0400

    updated js

--- assets/js/scripty.js
+++ assets/js/scripty.js
@@ -17,7 +17,7 @@ function loadDoc() {
   xhttp.onreadystatechange = function() {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
         var xml = xhttp.response;
-        var x = xml.getElementsByTagName("doc");
+        var x = xhttp.responseType;
         console.log(x);
         console.log(xml);
         
