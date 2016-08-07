43d4f2b4f20191232f22c2de4627904178b6b575
andyzim <andy.zim11@gmail.com>
2016-08-07 19:58:21 -0400

    added header

--- user_scripts/IodineGBACoreGlueCode.js
+++ user_scripts/IodineGBACoreGlueCode.js
@@ -118,7 +118,7 @@ var MixerInput = null;
 var timerID = null;
 window.onload = function () {
     if (!games[location.hash.substr(1)]) {
-        alert("Invalid game request!\nPlease choose a game from the 'Games' dropdown.\nEnjoy!);
+        alert("Invalid game request!\nPlease choose a game from the 'Games' dropdown.\nEnjoy!");
         return;
     }
     //Initialize Iodine:
